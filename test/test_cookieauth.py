1451c7a2e9846dee1a64d1ec66a2271b0212d838
Matthew Baker <mattheosb@gmail.com>
2024-09-19 11:48:22 +0200

    Added cookie auth test

--- test/test_cookieauth.py
+++ test/test_cookieauth.py
@@ -1,9 +1,10 @@
 import unittest
 from crossauth_backend.storageimpl.inmemorystorage import InMemoryKeyStorage
-from crossauth_backend.cookieauth import SessionCookie
+from crossauth_backend.cookieauth import SessionCookie, DoubleSubmitCsrfToken
+from crossauth_backend.crypto import Crypto
 from nulltype import NullType
 
-class SqlAlchemyKeyStorageTest(unittest.IsolatedAsyncioTestCase):
+class SessionKeyTest(unittest.IsolatedAsyncioTestCase):
 
     async def test_create_session_key(self):
         keyStorage = InMemoryKeyStorage()
@@ -18,3 +19,10 @@ class SqlAlchemyKeyStorageTest(unittest.IsolatedAsyncioTestCase):
         print("Expires", key["expires"], ret.expires)
         self.assertTrue(key["expires"] == ret.expires) # type: ignore
         self.assertTrue(key["expires"] > ret.created) # type: ignore
+
+class CsrfCookieTest(unittest.IsolatedAsyncioTestCase):
+    secret = "ABCDEFGHIJKLMNOPQRSTUVWX"
+    auth = DoubleSubmitCsrfToken({"secret": secret})
+    token = auth.create_csrf_token()
+    cookie = auth.make_csrf_cookie(token)
+    cookie_token = Crypto.unsign_secure_token(cookie["value"], secret)
